2e0d99271134d97f8ec70100dc2caef65d8dad5c
woojin_kim <mw7895la@gmail.com>
2022-01-10 16:51:15 +0900

    initial commit

---
+++ src/main/java/com/example/practice_react/PracticeReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeReactApplication.class, args);
+    }
+
+}

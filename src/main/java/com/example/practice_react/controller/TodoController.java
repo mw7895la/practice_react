4e4778530632ab10c11f40b53a07266fbd3d90eb
woojin_kim <mw7895la@gmail.com>
2022-01-11 15:29:57 +0900

    [New Feature] CRUD 추가함.

--- src/main/java/com/example/practice_react/controller/TodoController.java
+++ src/main/java/com/example/practice_react/controller/TodoController.java
@@ -1,15 +1,16 @@
 package com.example.practice_react.controller;
 
 import com.example.practice_react.dto.ResponseDTO;
+import com.example.practice_react.dto.TodoDTO;
+import com.example.practice_react.model.TodoEntity;
 import com.example.practice_react.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -20,7 +21,7 @@ public class TodoController {
     private TodoService todoService;
 
     @GetMapping("/test_prac")
-    public ResponseEntity<?> testTodo_prac(){
+    public ResponseEntity<?> testTodo_prac() {
         List<String> testList;
         testList = todoService.testService_prac();
 
@@ -29,12 +30,90 @@ public class TodoController {
     }
 
     @GetMapping("/test")
-    public ResponseEntity<?> testTodo(){
+    public ResponseEntity<?> testTodo() {
         String str = todoService.testService();
         List<String> list = new ArrayList<>();
         list.add(str);
 
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping            //ResponseEntity 는 사용자의 HttpRequest에 대한 응답 데이터를 포함하는 클래스이다.
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+        try {
+            //임시 아이디
+            String temp = "temp-user";
+
+            //★★★사용자로부터 요청이 들어올 때 해당 DTO를 Entity로 변환한다.
+            TodoEntity entity = TodoDTO.toEntity(dto);
+
+            //id를 null로 초기화한다.      //유저가 아닌 오브젝트 id이다.
+            entity.setId(null);
+
+            //임시 사용자 아이디를 넣어준다.
+            entity.setUserId(temp);
+
+            //★★★서비스를 이용해 Todo Entity를 생성한다.
+            List<TodoEntity> entities = todoService.create(entity);
+
+            //자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+            //map은 Stream에서 뽑아져 나오는 데이터에 변경을 가해준다.
+            //아래는 TodoEntity로 된 데이터들을 TodoDTO::new 로 DTO객체로 바꿔서 .collect에서 toList 즉, List컬렉션에 모아둔다.
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            //변환된 TodoDTO 리스트를 이용해 이제 최종적으로 ResponseDTO를 초기화 하고 클라이언트에게 리턴해준다.
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+        } catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> retrieveTodoList() {
+        String temp = "temp-user";
+
+        //서비스에서 retrieve 메서드를 이용해  Todo 리스트를 가져온다
+        List<TodoEntity> entities = todoService.retrieve(temp);
+
+        //TodoEntity를 TodoDTO로 변환해야지!
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        //변환된 TodoDTO 의 dtos를 이용해 responseDTO를 초기화 한다.
+        ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+        //이제 반환해준다.
+        return ResponseEntity.ok().body(response);
+
+
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateTodoList(@RequestBody TodoDTO dto){
+
+        //클라이언트가 업데이트를 하기위해 요청을 보내온다.
+        String temp = "temp-user";
+
+        //dto를 TodoEntity로 변환한다.
+        TodoEntity entity = TodoDTO.toEntity(dto);
+
+        //변환 후에 setUserid 설정
+        entity.setUserId(temp);
+
+        //서비스를 이용해 entity를 업데이트 한다
+        List<TodoEntity> entities = todoService.update(entity);
+
+        //업데이트 된 것을 TodoDTO로 변환한다.
+        List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+        //변환된 TodoDTO 리스트를 이용해서 responseDTO를 초기화 한다.
+        ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+        //ResponseDTO를 클라이언트에게 리턴한다.
         return ResponseEntity.ok().body(response);
     }
 }
--- src/main/java/com/example/practice_react/service/TodoService.java
+++ src/main/java/com/example/practice_react/service/TodoService.java
@@ -2,12 +2,15 @@ package com.example.practice_react.service;
 
 import com.example.practice_react.model.TodoEntity;
 import com.example.practice_react.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Slf4j      //Slf4j를 사용하려면 구현부를 연결해줘야 하는데 스프링이 기본적으로 해줌. 스프링이 기본적으로 사용하는 로그 라이브러리는 Logback이다.
 @Service
 public class TodoService {
 
@@ -34,5 +37,59 @@ public class TodoService {
         //TodoEntity 검색
         TodoEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+
+    //Service의 Create 부분.  repository에 저장하고 컨트롤러로 리턴한다.
+    public List<TodoEntity> create(final TodoEntity entity){
+        validate(entity);
+        repository.save(entity);
+        log.info("entity id : {} is saved.", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    public List<TodoEntity> retrieve(final String userId){
+        log.info("Todo List를 보기 위한 Service : {}", userId);
+        return repository.findByUserId(userId);
+    }
+
+    //update를 위한 Service
+    public List<TodoEntity> update(final TodoEntity entity){
+        //업데이트 하기 전, 유효한지부터 확인한다
+        validate(entity);
+
+        //유효하면 넘겨받은 entity의 id를 이용해  TodoEntity를 가져온다. 존재하지 않으면 업뎃 불가.
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+
+        //ifPresent 메소드로 값이 존재하면 덮어씌운다.
+        original.ifPresent(woojin ->{
+            woojin.setTitle(entity.getTitle());
+            woojin.setDone(entity.isDone());
+            //데이터베이스에 새 값을 저장한다.
+            repository.save(woojin);
+        });
+
+        return retrieve(entity.getUserId());
+
+    }
+
+
+
+    //entity 객체에 대한 유효성 검사. validate
+    private void validate(final TodoEntity entity){
+        if(entity==null){
+            log.warn("Entity cannot be null");
+            throw new RuntimeException("entity cannot be null");
+        }
+
+        if(entity.getUserId()==null){
+            log.warn("Unknown User");
+            throw new RuntimeException("Unknown User");
+        }
+
+        log.info("validate success");
+
     }
 }
